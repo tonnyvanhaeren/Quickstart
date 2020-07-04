738d66201e97df152cadc7f3f3c366ea1f7d7a33
Antonius Vanhaeren <antonius.vanhaeren@@telenet.be>
2020-07-04 10:26:53 +0200

    added Api Authorization policy

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -29,6 +29,16 @@ namespace Api
                         ValidateAudience = false
                     };
                 });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "api1");
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,7 +58,8 @@ namespace Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllers()
+                    .RequireAuthorization("Apiscope");
             });
         }
     }
